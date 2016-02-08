70a0e22a305cb2b9719c9409bb2cf395b303b533
root <risul@regfire.com>
2016-02-08 23:21:40 +0100

    Update to momentjs 2.11.2

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
     name: 'risul:moment-timezone',
     summary: 'Timezone support for moment.js, packaged for Meteor',
-    version: '0.5.0_4',
+    version: '0.5.0_5',
     git: 'https://github.com/risul/meteor-moment-timezone'
 });
 
 Package.on_use(function (api, where) {
     var where = where || ['client', 'server'];
-    var momentPackageName = !!Package.onUse ? "momentjs:moment@2.11.1" : "moment"
+    var momentPackageName = !!Package.onUse ? "momentjs:moment@2.11.2" : "moment"
     
     api.use(momentPackageName, where);
     
